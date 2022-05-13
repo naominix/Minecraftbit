6d8f25151b7a3393f9e2ef61b9099808dab99657
Hisashi <naotan@gmail.com>
2022-05-13 22:51:33 +0900

    First commit

---
+++ custom.ts
@@ -0,0 +1,119 @@
+/**
+ * Minecraft:bit Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace serialEvent {
+    let isConnected: boolean = false;
+    let recvTxt = ""
+
+    /**
+     * Connect Minecraft:bit via USB-Serial
+     */
+    //% blockId="mcbit_connect" block="connect to Minecraft:bit"
+    //% weight=100 color=#000080 blockGap=20
+    export function connect(): void {
+        serial.redirectToUSB()
+        isConnected = true
+        serial.setBaudRate(BaudRate.BaudRate115200)
+        basic.pause(100)
+    }
+
+    /**
+         * Write USB-Serial
+         */
+    //% blockId="serial_write" block="Write to USB-Serial"
+    //% weight=100 blockGap=20
+    export function Write(): void {
+        // serial.writeLine("custom block")
+        basic.pause(100)
+    }
+
+    /**
+     * TODO: describe your function here
+     * @param value describe value here, eg: 5
+     */
+    //% weight=50
+    //% block="ブロックを壊したとき"
+    //% draggableParameters
+    export function onBlockBroken(cb: () => void) {
+        control.onEvent(11, 22, function () {
+            cb()
+        })
+    }
+
+    /**
+     * TODO: describe your function here
+     * @param value describe value here, eg: 5
+     */
+    //% weight=50
+    //% block="ブロックを置いたとき"
+    //% draggableParameters
+    export function onBlockPlaced(cb: () => void) {
+        control.onEvent(12, 22, function () {
+            cb()
+        })
+    }
+
+    /**
+     * TODO: describe your function here
+     * @param value describe value here, eg: 5
+     */
+    //% weight=50
+    //% block="アイテムを捨てたとき"
+    //% draggableParameters
+    export function onItemDropped(cb: () => void) {
+        control.onEvent(13, 22, function () {
+            cb()
+        })
+    }
+
+    //% block block="アイテムを使ったとき"
+    export function onItemUsed(cb: () => void): void {
+        control.onEvent(14, 22, function () {
+            cb()
+        })
+    }
+
+    //% block block="プレイヤーがテレポートしたとき"
+    export function onTeleported(cb: () => void): void {
+        control.onEvent(15, 22, function () {
+            cb()
+        })
+    }
+
+    //% block block="プレイヤーが歩いたとき"
+    export function onTravelled(cb: () => void): void {
+        control.onEvent(16, 22, function () {
+            cb()
+        })
+    }
+
+    basic.forever(() => {
+        recvTxt = serial.readString()
+        switch (recvTxt) {
+            case 'BlockBroken':
+                control.raiseEvent(11, 22)
+                break;
+            case 'BlockPlaced':
+                control.raiseEvent(12, 22)
+                break;
+            case 'ItemDropped':
+                control.raiseEvent(13, 22)
+                break;
+            case 'ItemUsed':
+                control.raiseEvent(14, 22)
+                break;
+            case 'PlayerTeleported':
+                control.raiseEvent(15, 22)
+                break;
+            case ' ':
+                control.raiseEvent(16, 22)
+                break;
+            default:
+
+        }
+        basic.pause(50);
+    })
+
+
+}
